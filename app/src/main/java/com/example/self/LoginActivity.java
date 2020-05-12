7113c13a1b43e8a80025018e61db0e1d66617e17
SULTAN <ramhziworld@gmail.com>
2020-05-12 09:38:04 +0100

    Added button instance and functionality

--- app/src/main/java/com/example/self/LoginActivity.java
+++ app/src/main/java/com/example/self/LoginActivity.java
@@ -2,13 +2,37 @@ package com.example.self;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button loginButton,createAccountButtonLogin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginButton = findViewById(R.id.email_sign_in_button);
+        createAccountButtonLogin = findViewById(R.id.create_account_button_login);
+        loginButton.setOnClickListener(this);
+        createAccountButtonLogin.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId())
+        {
+            case R.id.email_sign_in_button:
+                //
+                break;
+            case R.id.create_account_button_login:
+                startActivity(new Intent(LoginActivity.this,CreateAccountActivity.class));
+                break;
+        }
+
     }
 }
